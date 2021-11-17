5adf3a656e576b304b9779d152152de6ce89c36b
FaJey1 <pstsyngalev@mail.ru>
2021-11-17 09:14:29 +0300

    Добавлена 18, 19 задачи

--- src/ru/mirea/task11/MouseEvent/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module ru.mirea.task11.mouseevent {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires jdk.accessibility;
-
-
-    opens ru.mirea.task11.mouseevent to javafx.fxml;
-    exports ru.mirea.task11.mouseevent;
-}
--- src/ru/mirea/task9/tk9/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module ru.mirea.task9.tk9 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens ru.mirea.task9.tk9 to javafx.fxml;
-    exports ru.mirea.task9.tk9;
-}
