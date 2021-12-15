46bdc36b4393d79fd068497ed9e353704ebfa78d
FaJey1 <pstsyngalev@mail.ru>
2021-12-15 16:47:45 +0300

    Вся жаба

--- src/ru/mirea/task22/SpecialServicesWork.java
+++ src/ru/mirea/task22/SpecialServicesWork.java
@@ -1,18 +1,14 @@
 package ru.mirea.task22;
 
 public class SpecialServicesWork {
+
+    private final Factory specialistFactory;
+
+    public SpecialServicesWork(Factory specialistFactory) {
+        this.specialistFactory = specialistFactory;
+    }
     public SpecialServices orderSpecialServices(SpecialServicesType type, int id){
-        SpecialServices specialServicesType = null;
-        switch (type) {
-            case COLLECTORS: {
-                specialServicesType = new Collectors();
-                break;
-            }
-            case BAILIFF: {
-                specialServicesType = new Bailiff();
-                break;
-            }
-        }
+        SpecialServices specialServicesType = specialistFactory.createSpecialist(type);
         specialServicesType.getDebtor(id);
         specialServicesType.collectMoney();
         System.out.println("Должник погасил задолженность!");
